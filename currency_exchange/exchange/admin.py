325eb5546900ae3eff0824adcf20858545c61178
Dawid Konrad Kohnke <dawid.kohnke.cad@gmail.com>
2024-11-11 19:13:56 +0100

    Add admin panel
    for listing Currencies and ExchangeRate

--- currency_exchange/exchange/admin.py
+++ currency_exchange/exchange/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Currency, ExchangeRate
 
-# Register your models here.
+
+@admin.register(Currency)
+class CurrencyAdmin(admin.ModelAdmin):
+    list_display = 'code',
+
+
+@admin.register(ExchangeRate)
+class ExchangeRateAdmin(admin.ModelAdmin):
+    list_display = ('from_currency', 'to_currency', 'rate', 'date')
+    list_filter = ('from_currency', 'to_currency', 'date')
